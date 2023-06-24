b61eeba54112cb39a1f8ac3ae67532e311ab9061
Stéphanie Fernandes <108842254+Ste-nandes@users.noreply.github.com>
2023-06-24 14:25:40 -0300

    Add files via upload

---
+++ youtube/estrutura while/scripts.js
@@ -0,0 +1,15 @@
+for(var i = 0; i < 10; i++){ //for é praticamente a mesma coisa que while, a única diferença é que For é escrito tudo na mesma linha, sendo mais prático.
+    console.log("testando for: " + i);
+}
+
+var arr = [1,2,3,4];
+
+for(var s = 0; s < arr.length; s++){ //arr.length significa comprimento. Ou seja, este array tem 4 elementos de comprimento
+    console.log(arr[j]);
+}
+
+console.log(arr.length);
+
+for(var x = 5; x < 100; x *= 3){
+    console.log(x);
+}
